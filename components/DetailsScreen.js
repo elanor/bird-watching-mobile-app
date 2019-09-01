109668c5473be52a236ed5c0513e3fcb1244f428
Nikita Popov <losnikitos@yandex-team.ru>
2019-09-01 23:59:37 +0300

    Birds storage

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,220 +1,149 @@
-import React, { Component } from 'react';
-import { 
-  Button, View, Text, ScrollView, TextInput, TouchableOpacity, Keyboard, Picker, Image, AsyncStorage, StyleSheet} from 'react-native';
+import React, { Component } from "react";
+import {
+  Button,
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  Picker,
+  Image,
+  AsyncStorage,
+  StyleSheet
+} from "react-native";
 
-//import { addBird } from '../src/storage/dataStorage';
+import CameraRoll from "expo";
 
-// import styles from '../App';
-
-
-import CameraRoll from 'expo';
-import HomeScreen from './HomeScreen';
-// import console = require('console');
-
-const asyncId = 'bird-1234567890-2';
-const STORAGE_KEY = 'BIRD_DATAS';
-/* const getBirdArray =  async () => {
-  var array = []
-  try {
-    array += JSON.parse(await AsyncStorage.getItem('asyncId') || 'none');
-  } catch (error) {
-    // Error retrieving data
-    console.log(error.message);
-  }
-  return array;
-} */
-/* const getBirdArray = async () => {
-  try {
-    let birdDatas = await AsyncStorage.getItem(STORAGE_KEY);
-
-    if (birdDatas === null) { return []; }
-
-    return parseBirdDatas(birdDatas);
-  } catch (error) {
-    console.log('Error fetching Bird Datas - parse', error);
-  }
-}
- */
-const fetchBirdDatas = async () => {
-  try {
-    let birdDatas = await AsyncStorage.getItem(STORAGE_KEY);
-
-    if (birdDatas === null) { return []; }
-
-    return parseBirdDatas(birdDatas);
-  } catch (error) {
-    console.log('Error fetching BirdDatas in DetailsScreen', error);
-  }
-}
-
-const parseBirdDatas = (birdDatas) =>
-  JSON.parse(birdDatas).map((birdData) => {
-    birdData.rarity = new birdRarity(birdData.rarity)
-    birdData.name = new birdName(birdData.name)
-    
-    // birdData.createdAt = new Date(birdData.createdAt)
-    return birdData;
-  });
-
-const saveBirdDatas = (birdDatas) => {
-  AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(birdDatas));
-}
-
-const updateBirdDatas = async (birdArray) => {
-  try {
-    let birdDatas = await fetchBirdDatas();
-    birdDatas = mergeBirdDatas(birdDatas, birdArray);
-    saveBirdDatas(birdDatas);
-
-    this.setState({ birdDatas });
-
-    console.log('Bird Datas', this.state.birdDatas);
-  } catch (error) {
-    console.log('Error fetching Bird Datas', error);
-  }
-}
-
-
-const mergeBirdDatas = (birdDatas, birdArray) => {
-  const score = {
-    score: birdArray,
-    createdAt: new Date()
-  };
-
-  return [...birdDatas, score];
-}
-
-
-const saveBird = async inBirdArray => {
-    AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(inBirdArray));
-  }
+import dataStorage from "../storage/dataStorage";
 
 class DetailsScreen extends Component {
-  birdName = ""
-  birdRarity = ""
-  birdGeolocation= ""
-  birdImagelink= ""
-  birdComment = ""
-  birdTimestamp = ""
+  birdName = "";
+  birdRarity = "";
+  birdGeolocation = "";
+  birdImagelink = "";
+  birdComment = "";
+  birdTimestamp = "";
 
   constructor(props) {
     super(props);
-    this.handleNameChange = this.handleNameChange.bind(this); 
+    this.handleNameChange = this.handleNameChange.bind(this);
     this.handleRarityChange = this.handleRarityChange.bind(this);
     this.handleCommentChange = this.handleCommentChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
     this.state = {
-      //defauilt value of the date time
-      name: '',
-      rarity: '',
-      comment: '',
-      date: '',
+      //default value of the date time
+      name: "",
+      rarity: "",
+      comment: "",
+      date: ""
     };
-    //this.handleAddPhotosChange = this.handleAddPhotosChange.bind(this);
 
-  this.birdArray = [];
+    this.birdArray = [];
     this.birdArray = this.birdArray + fetchBirdDatas();
-    console.log("array content: " + this.birdArray)
+    console.log("array content: " + this.birdArray);
 
     // here goes adding pictures
     handleAddPhotosButtonPress = () => {
-      console.log("handle_add_photo_change works")
+      console.log("handle_add_photo_change works");
       CameraRoll.getPhotos({
         first: 20,
-        assetType: 'Photos',
+        assetType: "Photos"
       })
 
-      .then(r => {
-        this.setState({ imagelink: r.edges });
-        this.birdImagelink = imagelink;
-      })
-      .catch((err) => {
-        //Error Loading Images
-      });
+        .then(r => {
+          this.setState({ imagelink: r.edges });
+          this.birdImagelink = imagelink;
+        })
+        .catch(err => {
+          //Error Loading Images
+        });
     };
-        
   }
 
   componentDidMount() {
-    var date = new Date().getDate(); 
-    var month = new Date().getMonth() + 1; 
-    var year = new Date().getFullYear(); 
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
     var hours = new Date().getHours();
-    var min = new Date().getMinutes(); 
+    var min = new Date().getMinutes();
 
-    this.setState({      
-      date:
-        date + '/' + month + '/' + year + ' ' + hours + ':' + min,
+    this.setState({
+      date: date + "/" + month + "/" + year + " " + hours + ":" + min
     });
   }
 
   static navigationOptions = {
-    title: 'Details',
+    title: "Details"
   };
 
-    handleNameChange(name) {
-      console.log("handleNameChange");
-      this.setState({ name });
-      this.birdName = name;
-    }
+  handleNameChange(name) {
+    console.log("handleNameChange");
+    this.setState({ name });
+    this.birdName = name;
+  }
 
-  // here goes rarity  
+  // here goes rarity
   handleRarityChange(rarity, index) {
-    console.log("handleRarityChange: "+rarity+" i:"+index);
+    console.log("handleRarityChange: " + rarity + " i:" + index);
     this.birdRarity = rarity;
   }
 
   // here goes comment change
-  handleCommentChange(comment){
+  handleCommentChange(comment) {
     console.log("handleCommentChange");
     this.setState({ comment });
     this.birdComment = comment;
   }
-  
+
   handleSubmit() {
     console.log("handleSubmit save button");
-    //saveSettings(this.state);
-    if(!this.birdName.length>0){
-      alert("Any bird should have a name!")
+    if (!this.birdName.length > 0) {
+      alert("Any bird should have a name!");
       return;
     }
-    newBird = {
-      name: this.birdName,
-      rarity: this.birdRarity,
-      //imagelink: this.birdImagelink,
-      comment: this.birdComment,
-      timestamp: this.birdTimestamp
-    } 
 
-    this.birdArray[this.birdArray.length] = newBird;
-    console.log("handleSubmit add new bird: " + this.birdArray.toString() );
-    updateBirdDatas(this.birdArray);
+    // все поля птицы записаны в this.state, то есть this.state и есть описание птички
+    const newBird = this.state;
+
+    // сохраняем в хранилище
+    dataStorage.addBird(newBird);
+
+    console.log("handleSubmit add new bird: " + this.birdArray.toString());
     console.log("handleSubmit bird array length:" + this.birdArray.length);
     console.log("handleSubmit save bird :" + this.birdArray);
   }
 
-render() {
-  console.log("DetailsScreen render");
+  render() {
+    console.log("DetailsScreen render");
     return (
       <View style={styles.container}>
-        <ScrollView> 
+        <ScrollView>
+          <Image
+            style={{
+              width: 150,
+              height: 150,
+              marginTop: 8,
+              alignSelf: "center"
+            }}
+            source={{
+              uri:
+                "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg"
+            }}
+          />
 
-            <Image
-              style={{width: 150, height: 150, marginTop: 8, alignSelf: "center"}}              
-              source={{uri: 'https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg'}}
-            />
+          <TouchableOpacity
+            style={styles.saveButton}
+            onPress={() => this.handleAddPhotosButtonPress}
+          >
+            <Text style={styles.saveButtonText}> Load image </Text>
+          </TouchableOpacity>
 
-            <TouchableOpacity 
-              style = {styles.saveButton}              
-              onPress={() => this.handleAddPhotosButtonPress} >            
-              <Text style = {styles.saveButtonText}> Load image </Text>
-            </TouchableOpacity>
-
-                <ScrollView>
-                  {this.state.imagelink}
-                 {/*  to fix this!!! */}
-                 {/*  {this.state.imagelink.map((p, i) => {
+          <ScrollView>
+            {this.state.imagelink}
+            {/*  to fix this!!! */}
+            {/*  {this.state.imagelink.map((p, i) => {
                   return (
                     <Image
                       key={i}
@@ -226,64 +155,61 @@ render() {
                     />
                   );
                 })} */}
-                </ScrollView>
-        
-            <TextInput 
-              style={styles.textInput}
-              fontSize = {18}
-              placeholder="Species' name"
-              maxLength={20}
-              onBlur={Keyboard.dismiss}
-              returnKeyType="done"
-              value={this.birdName}
-              onChangeText={this.handleNameChange}
-            />
+          </ScrollView>
 
-            <Text style = {styles.text}>Choose rarity</Text>
-            
-            <Picker style = {styles.textInput}
-              selectedValue={this.birdRarity}
-              placeholder="Choose rarity"
-              onValueChange={(itemValue, itemIndex) => {
-                this.setState({itemValue});
-                this.handleRarityChange(itemValue, itemIndex)
-                
-                }
-              }>
-              <Picker.Item label="Common" value="Common"/>
-              <Picker.Item label="Rare" value="Rare" />
-              <Picker.Item label="Extremely rare" value="ExtremelyRare"/>
-            </Picker>
+          <TextInput
+            style={styles.textInput}
+            fontSize={18}
+            placeholder="Species' name"
+            maxLength={20}
+            onBlur={Keyboard.dismiss}
+            returnKeyType="done"
+            value={this.birdName}
+            onChangeText={this.handleNameChange}
+          />
 
-            <TextInput
-              style={styles.textInputContainer}
-              style = {styles.textInput}
-              itemStyle = "baseline"
-              multiline = {true}
-              numberOfLines = {4}
-              placeholder="Comment"
-              maxLength={170}
-              returnKeyType="done"
-              onBlur={Keyboard.dismiss}
-              value={this.birdComment}
-              onChangeText={this.handleCommentChange}
-            />
+          <Text style={styles.text}>Choose rarity</Text>
 
-          <Text
-            style={styles.text}>Timestamp: {this.state.date}
-          </Text>
-
-      
-      </ScrollView>  
-        
-          <TouchableOpacity style = {styles.saveButton}
-            raised = {true}
-            onPress={this.handleSubmit}
+          <Picker
+            style={styles.textInput}
+            selectedValue={this.birdRarity}
+            placeholder="Choose rarity"
+            onValueChange={(itemValue, itemIndex) => {
+              this.setState({ itemValue });
+              this.handleRarityChange(itemValue, itemIndex);
+            }}
           >
-            <Text style = {styles.saveButtonText}> Save </Text>
-          </TouchableOpacity>     
-            
-         {/*  <TouchableOpacity style = {styles.saveButton}   
+            <Picker.Item label="Common" value="Common" />
+            <Picker.Item label="Rare" value="Rare" />
+            <Picker.Item label="Extremely rare" value="ExtremelyRare" />
+          </Picker>
+
+          <TextInput
+            style={styles.textInputContainer}
+            style={styles.textInput}
+            itemStyle="baseline"
+            multiline={true}
+            numberOfLines={4}
+            placeholder="Comment"
+            maxLength={170}
+            returnKeyType="done"
+            onBlur={Keyboard.dismiss}
+            value={this.birdComment}
+            onChangeText={this.handleCommentChange}
+          />
+
+          <Text style={styles.text}>Timestamp: {this.state.date}</Text>
+        </ScrollView>
+
+        <TouchableOpacity
+          style={styles.saveButton}
+          raised={true}
+          onPress={this.handleSubmit}
+        >
+          <Text style={styles.saveButtonText}> Save </Text>
+        </TouchableOpacity>
+
+        {/*  <TouchableOpacity style = {styles.saveButton}   
             onPress={() => this.props.navigation.push('Details')}
           >
             <Text style = {styles.saveButtonText}> Go to Details... again </Text>
@@ -294,15 +220,14 @@ render() {
           >
             <Text style = {styles.saveButtonText}> Go to Home </Text>
           </TouchableOpacity> */}
-            
-          <TouchableOpacity style = {styles.saveButton}            
-            onPress={() => this.props.navigation.navigate('Home')}
-          >
-            <Text style = {styles.saveButtonText}> Cancel </Text>
-          </TouchableOpacity>
 
-              
-    </View>   
+        <TouchableOpacity
+          style={styles.saveButton}
+          onPress={() => this.props.navigation.navigate("Home")}
+        >
+          <Text style={styles.saveButtonText}> Cancel </Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
@@ -310,14 +235,13 @@ render() {
 export default DetailsScreen;
 
 const styles = StyleSheet.create({
-
   container: {
     flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "space-around",
+    alignItems: "stretch",
+    backgroundColor: "#F5FCFF",
     paddingBottom: 4,
-  
+
     paddingStart: 4,
     paddingEnd: 4
   },
@@ -326,33 +250,33 @@ const styles = StyleSheet.create({
     marginTop: 8,
     paddingStart: 24,
     fontSize: 18,
-    alignItems: 'center'    
+    alignItems: "center"
   },
-  
+
   textInput: {
-    borderColor: '#CCCCCC',
+    borderColor: "#CCCCCC",
     marginTop: 8,
     marginStart: 16,
     marginEnd: 16,
     paddingStart: 8,
     borderWidth: 1,
     fontSize: 18,
-    alignItems: 'flex-start'    
+    alignItems: "flex-start"
   },
 
   textInputContainer: {
     height: 150,
     paddingLeft: 20,
     paddingRight: 20,
-    alignItems: 'stretch'
+    alignItems: "stretch"
   },
 
   saveButton: {
     borderWidth: 1,
-    borderColor: '#007BFF',
-    backgroundColor: '#007BFF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: "#007BFF",
+    backgroundColor: "#007BFF",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
     marginTop: 12,
     marginBottom: 12,
@@ -363,8 +287,8 @@ const styles = StyleSheet.create({
   },
 
   saveButtonText: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 18,
-    textAlign: 'center'
-  },
+    textAlign: "center"
+  }
 });
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,76 +1,82 @@
-import React, { Component } from 'react';
-import { View, Text, ListView, ListItem, TouchableOpacity, StyleSheet, ScrollView, AsyncStorage, Button } from 'react-native'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  ListView,
+  ListItem,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  AsyncStorage,
+  Button
+} from "react-native";
 
-
-
-//const asyncId = 'bird-1234567890-1';
-
-const STORAGE_KEY = 'BIRD_DATAS';
-
-const parseBirdDatas = (birdDatas) =>
-  JSON.parse(birdDatas).map((birdData) => {    
-    //birdData.createdAt = new Date(birdData.createdAt)
-    birdData.rarity = new birdRarity(birdData.rarity)
-    birdData.name = new birdName(birdData.name)
-    return birdData;
-  });
-  
-const getBirdArray = async () => {
-  try {
-    let birdDatas = await AsyncStorage.getItem(STORAGE_KEY);
-    if (birdDatas === null) { return []; }
-    return parseBirdDatas(birdDatas);
-  } catch (error) {
-    console.log('Error fetching Bird Datas in HomeScreen', error);
-  }
-}
+import dataStorage from "../storage/dataStorage";
 
 class HomeScreen extends Component {
-
   static navigationOptions = {
-    title: 'Recorded Species',
+    title: "Recorded Species"
   };
+
+  // инициализировали state пустым массивом
+  state = {
+    birds: []
+  };
+
+  // когда компонент создан, загружаем данные
+  async componentDidMount() {
+    try {
+      // когда обновится список птиц, вызовется this.setState и компонент перерисуется
+      dataStorage.onBirdsUpdated = (birds) => this.setState({ birds });
+
+      // запускаем загрузку птичек из localStorage
+      await dataStorage.loadBirds();
+
+    } catch (e) {
+      console.log("Could not load birds from local storage");
+    }
+  }
 
   constructor(props) {
     super(props);
     console.log("HomeScreen constructor");
-    //this.birdArray = [];
-    this.birdArray = getBirdArray();
-    console.log("array["+ this.birdArray.length +"] content: " + this.birdArray)
-    console.log( this.createBirdListText() )
+
+    // тут всегда будет пусто, потому что когда запускается конструктор, данных еще нет
+    console.log(this.createBirdListText());
+
     //this.birdDataInstance = birddata;
     /*var birdArray = [
-      { id: 1, 
-        name: "Pigeon Mike", 
-        imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg", 
-        rarity: "philps", 
-        timestamp: "today", 
+      { id: 1,
+        name: "Pigeon Mike",
+        imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg",
+        rarity: "philps",
+        timestamp: "today",
         comment: "New York"
       },
-      { id: 2, 
-        name: "Pigeon Steve", 
-        imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg", 
-        rarity: "Square", 
-        timestamp: "today", 
+      { id: 2,
+        name: "Pigeon Steve",
+        imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg",
+        rarity: "Square",
+        timestamp: "today",
         comment: "Chicago"
       },
-      { id: 3, 
-        name: "Pigeon John", 
-        imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg", 
-        rarity: "market", 
-        timestamp: "today", 
+      { id: 3,
+        name: "Pigeon John",
+        imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg",
+        rarity: "market",
+        timestamp: "today",
         comment: "New York"
-      },   
-    ];  
+      },
+    ];
 
     console.log('main test array created');
 
-    var newBird = 
-    { id: 4, 
-      name: "Pigeon John", 
-      imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg", 
-      rarity: "market", 
-      timestamp: "today", 
+    var newBird =
+    { id: 4,
+      name: "Pigeon John",
+      imagelink: "https://www.allaboutbirds.org/guide/assets/photo/66031271-480px.jpg",
+      rarity: "market",
+      timestamp: "today",
       comment: "New York"
     };
 
@@ -80,28 +86,20 @@ class HomeScreen extends Component {
     console.log('item added to test array');
     */
   }
-  
-  createBirdListText() {
-    console.log("createBirdListText")
-    //var birdArray = birdDataInstance.getData()
-    var text = "\b"
 
-    for (var i=0; i < this.birdArray.length; i++) {
-      console.log("for loop");
-      text= text + "\n"+ i +". bird, name: "+ this.birdArray[i].name + " ";
-      
-    }
-    return text;
+  createBirdListText() {
+    // Читаем птиц из state компонента
+    const { birds } = this.state;
+    return birds.map((bird, index) => `${index}. bird, name: ${bird}`);
   }
 
   render() {
-    console.log("bird constructor");
+    console.log("HomeScreen render");
     return (
-        <View style={styles.container}>
-          <ScrollView >
+      <View style={styles.container}>
+        <ScrollView>
+          {/*
 
-            {/* 
-            
             <Button
               icon={{
                   name: "sort"
@@ -125,15 +123,17 @@ class HomeScreen extends Component {
                 console.log('sorting button pressed')}}
             />  */}
 
-            <Text style = {styles.text}>Here goes list of species: {this.createBirdListText()}</Text>
-            <TouchableOpacity 
-              style = {styles.saveButton}
-              onPress={() => this.props.navigation.navigate('Details')}
-            >
-              <Text style = {styles.saveButtonText}>Add new</Text>
-            </TouchableOpacity>
-          </ScrollView>
-        </View>      
+          <Text style={styles.text}>
+            Here goes list of species: {this.createBirdListText()}
+          </Text>
+          <TouchableOpacity
+            style={styles.saveButton}
+            onPress={() => this.props.navigation.navigate("Details")}
+          >
+            <Text style={styles.saveButtonText}>Add new</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
     );
   }
 }
@@ -141,33 +141,33 @@ class HomeScreen extends Component {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-
   item: {
     padding: 10,
     fontSize: 18,
-    height: 44,
-    },
+    height: 44
+  },
 
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
 
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5},
-    inputContainer: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  },
+  inputContainer: {
     paddingTop: 15
   },
 
   container: {
     flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-    paddingBottom: 4,  
+    justifyContent: "space-around",
+    alignItems: "stretch",
+    backgroundColor: "#F5FCFF",
+    paddingBottom: 4,
     paddingStart: 4,
     paddingEnd: 4
   },
@@ -176,33 +176,33 @@ const styles = StyleSheet.create({
     marginTop: 8,
     paddingStart: 24,
     fontSize: 18,
-    alignItems: 'center'    
+    alignItems: "center"
   },
-  
+
   textInput: {
-    borderColor: '#CCCCCC',
+    borderColor: "#CCCCCC",
     marginTop: 8,
     marginStart: 16,
     marginEnd: 16,
     paddingStart: 8,
     borderWidth: 1,
     fontSize: 18,
-    alignItems: 'flex-start'    
+    alignItems: "flex-start"
   },
 
   textInputContainer: {
     height: 150,
     paddingLeft: 20,
     paddingRight: 20,
-    alignItems: 'stretch'
+    alignItems: "stretch"
   },
 
   saveButton: {
     borderWidth: 1,
-    borderColor: '#007BFF',
-    backgroundColor: '#007BFF',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: "#007BFF",
+    backgroundColor: "#007BFF",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 10,
     marginTop: 12,
     marginBottom: 12,
@@ -213,8 +213,8 @@ const styles = StyleSheet.create({
   },
 
   saveButtonText: {
-    color: '#FFFFFF',
+    color: "#FFFFFF",
     fontSize: 18,
-    textAlign: 'center'
-  },
+    textAlign: "center"
+  }
 });
